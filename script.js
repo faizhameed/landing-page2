bfbe095431b554479f010ff643a34a0f643a934b
faizhameed <faizftb@gmail.com>
2019-08-06 17:35:36 +0530

    Mobile view. dektop view blocked for time

--- script.js
+++ script.js
@@ -1,7 +1,36 @@
 const nav = document.getElementById('navbar');
 
 const scrollHandle=()=>{
+    if(nav)
     window.scrollY>25?nav.classList.add("navShadow"):nav.classList.remove("navShadow");
 }
 
 window.addEventListener("scroll",scrollHandle)
+
+/* var interval = setInterval(function() {
+          // method to be executed;
+          console.log("faiz");
+        }, 500); */
+        $(document).ready(function(){
+            $('.customer-logos').slick({
+              slidesToShow: 6,
+              slidesToScroll: 1,
+              autoplay: true,
+              autoplaySpeed: 1000,
+              arrows: false,
+              dots: false,
+              pauseOnHover: false,
+              responsive: [{
+                breakpoint: 768,
+                settings: {
+                  slidesToShow: 4
+                }
+              }, {
+                breakpoint: 520,
+                settings: {
+                  slidesToShow: 3
+                }
+              }]
+            });
+          });
+      
