0ecfcc927831ef5b80e5861437015e074882321c
faizhameed <faizftb@gmail.com>
2019-08-17 18:10:29 +0530

    several changes

--- script.js
+++ script.js
@@ -43,4 +43,14 @@ window.addEventListener("scroll",scrollHandle)
               }]
             });
           });
-      
+      // **************************************************************
+      $("#slideshow > div:gt(0)").hide();
+
+      setInterval(function() {
+        $('#slideshow > div:first')
+          .fadeOut(1000)
+          .next()
+          .fadeIn(1000)
+          .end()
+          .appendTo('#slideshow');
+      }, 3000);
